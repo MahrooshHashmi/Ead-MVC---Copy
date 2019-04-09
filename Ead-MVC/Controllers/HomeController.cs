487c3650f16d048823e26a43e4c594fe8b599be9
mahroosh16hashmi <mahroosh16hashmi@hotmail.com>
2019-04-09 13:55:20 +0500

    Add project files.

---
+++ Ead-MVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ead_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+         
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult RegisterVendor()
+        {
+            return View();
+        }
+        public ActionResult RegisterCustomer()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+    }
+}
